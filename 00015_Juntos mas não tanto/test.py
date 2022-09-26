0733e880e0b38a735969e47a5520f05ee0f941ce
rocio@mumuki.org <rocio@mumuki.org>
2022-09-26 21:05:04 +0000

    Mumuki Export on 2022-09-26 21:05:04 +0000

--- 00015_Juntos mas não tanto/test.py
+++ 00015_Juntos mas não tanto/test.py
@@ -5,14 +5,14 @@ class Test(unittest.TestCase):
     
   
   def test_juntar_quadri_motor_retorna_quadrimotor(self):
-    self.assertEqual(juntar("", ["cuatri", "motor"]), "cuatrimotor")
+    self.assertEqual(juntar("", ["quadri", "motor"]), "quadrimotor")
     
   def test_juntar_olá_mundo_com_um_espaço_retorna_olá_mundo(self):
-    self.assertEqual(juntar(" ", ["hola", "mundo"]), "hola mundo")
+    self.assertEqual(juntar(" ", ["olá", "mundo"]), "olá mundo")
     
       
   def test_juntar_olá_mundo_com_uma_vírgula_retorna_olá_vírgula_mundo(self):
-    self.assertEqual(juntar(",", ["hola", "mundo"]), "hola,mundo")
+    self.assertEqual(juntar(",", ["olá", "mundo"]), "olá,mundo")
     
     
   
