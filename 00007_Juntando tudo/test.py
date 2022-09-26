34db9ca4a693cdcacdb840cd033f73ff7c45aa9e
rocio@mumuki.org <rocio@mumuki.org>
2022-09-26 20:58:56 +0000

    Mumuki Export on 2022-09-26 20:58:56 +0000

--- 00007_Juntando tudo/test.py
+++ 00007_Juntando tudo/test.py
@@ -1,11 +1,11 @@
 class Test(unittest.TestCase):
 
-  def test_juntar_super_califragilistico_espialidoso_devuelve_supercalifragilisticoespialidoso(self):
+  def test_juntar_super_califragilistico_espialidoso_retorna_supercalifragilisticoespialidoso(self):
     self.assertEqual(juntar(["super", "califragilistico", "espialidoso"]), "supercalifragilisticoespialidoso")
     
-  def test_juntar_lista_vacia_devuelve_string_vacio(self):
+  def test_juntar_lista_vazia_retorna_string_vazio(self):
     self.assertEqual(juntar([]), "")
     
   
-  def test_juntar_cuatri_motor_devuelve_cuatrimotor(self):
+  def test_juntar_quadri_motor_retorna_quadrimotor(self):
     self.assertEqual(juntar(["cuatri", "motor"]), "cuatrimotor")
