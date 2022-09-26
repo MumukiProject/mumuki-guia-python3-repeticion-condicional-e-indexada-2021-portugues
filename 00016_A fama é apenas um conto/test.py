78ca1d5d7103a0c38d2e24a6d42650315eaa7df2
rocio@mumuki.org <rocio@mumuki.org>
2022-09-26 21:06:37 +0000

    Mumuki Export on 2022-09-26 21:06:37 +0000

--- 00016_A fama é apenas um conto/test.py
+++ 00016_A fama é apenas um conto/test.py
@@ -1,8 +1,8 @@
 class Test(unittest.TestCase):
 
-  def test_cuantas_veces_entreno_lo_suficiente_con_35_40_32_60_es_4(self):
+  def test_quantas_vezes_treino_o_suficiente_com_35_40_32_60_é_4(self):
     self.assertEquals(quantas_vezes_eu_treino_o_suficiente([35, 40, 32, 60]), 4)
     
-  def test_cuantas_veces_entreno_lo_suficiente_con_15_45_90_0_es_2(self):
+  def test_quantas_vezes_treino_o_suficiente_com_15_45_90_0_é_2(self):
     self.assertEquals(quantas_vezes_eu_treino_o_suficiente([15, 45, 90, 0]), 2)
     
