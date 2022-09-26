c9d10aff4f239895abb94832da118ddb1fa0e751
rocio@mumuki.org <rocio@mumuki.org>
2022-09-26 20:57:04 +0000

    Mumuki Export on 2022-09-26 20:57:04 +0000

--- 00009_Todos os dias um pouco/test.py
+++ 00009_Todos os dias um pouco/test.py
@@ -1,8 +1,8 @@
 class Test(unittest.TestCase):
 
-  def test_todos_los_dias_un_poco_con_35_40_32_60_es_True(self):
+  def test_todos_os_dias_um_pouco_com_35_40_32_60_é_True(self):
     self.assertTrue(todos_os_dias_um_pouco([35, 40, 32, 60]))
     
-  def test_todos_los_dias_un_poco_con_15_45_90_0_es_False(self):
+  def test_todos_os_dias_um_pouco_com_15_45_90_0_é_False(self):
     self.assertFalse(todos_os_dias_um_pouco([15, 45, 90, 0]))
     
